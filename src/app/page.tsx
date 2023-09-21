a6d9316eb1f82c9a01c184bf36e6f6766f497b2f
IU-Imam <imamuddin48@gmail.com>
2023-09-21 06:35:51 +0600

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,8 +47,8 @@ export default function Home() {
           <AiFillLinkedin />
           <AiFillYoutube />
         </div>
-        <div className='relative mx-auto bg-gradient-to-b from-teal-100 rounded-full w-80 h-80 mt-20 overflow-hidden'>
-          <Image src={im} layout='fill' objectFit='fill' className='  px-2  ' />
+        <div className='relative mx-auto bg-gradient-to-b from-teal-100 rounded-full w-80 h-80 mt-20 overflow-hidden px-2'>
+          <Image src={im} alt='' layout='fill' objectFit='fill' />
         </div>
       </section>
     </main>
